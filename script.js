59939a731f5186e45a6af97efe5102580a0da451
AdrianoMathon <adrianomathon@hotmail.com>
2022-07-26 22:59:57 -0300

    desafio

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let num1,num2
+const convertir = (numero1, numero2) => numero1 / numero2
+
+let respuesta
+
 do {
-    num1 = parseFloat(prompt("Ingrese su sueldo en $"))
-    num2 = parseFloat(prompt("Ingrese cuanto cuesta un USD".replace(/,/, '.')))
-    if(num1 <= 0 || num2 <= 0 || isNaN(num1) || isNaN(num2) ) {
-        alert("Ingrese numeros validos")
-        break
-    }
-    if(num1 > 0 || num2 > 0){
-        alert(`Podes comprar ${num1 / num2} USD`)
-    }
+    let num1,num2
+    do {
+        num1 = parseFloat(prompt("Ingrese su sueldo en $")) // Sueldo en $
+        num2 = parseFloat(prompt("Ingrese cuanto cuesta un USD".replace(/,/, '.'))) // Cuanto cuesta un USD
+        if(num1 <= 0 || num2 <= 0 || isNaN(num1) || isNaN(num2) ) {    //Alerta por ingreso de numeros no válidos
+            alert("Ingrese numeros validos")
+            break
+        }
+        if(num1 > 0 || num2 > 0){
+            alert(`Podes comprar ${num1 / num2} USD`) // Devuelve valor
+        }
+    
+    } while(isNaN(num1) || isNaN(num2) || (num2 === 0))
+    
+respuesta = prompt("¿Quiere volver a calcular?").toLocaleLowerCase()
 
-} while(isNaN(num1) || isNaN(num2))
-
-console.log(num1 / num2)
+} while (respuesta != "no");
+    
+let resultado = convertir (num1, num2)
+console.log(resultado)
