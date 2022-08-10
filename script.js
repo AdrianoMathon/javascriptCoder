47a0e58dfd55cc59ce720b0a5363c3c273848e3a
AdrianoMathon <adrianomathon@hotmail.com>
2022-08-09 23:05:12 -0300

    primer entrega

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const convertir = (numero1, numero2) => numero1 / numero2
 
 let respuesta
@@ -23,3 +24,89 @@ respuesta = prompt("¿Quiere volver a calcular?").toLocaleLowerCase()
     
 let resultado = convertir (num1, num2)
 console.log(resultado)
+
+*/
+
+
+const productos_definidos = [
+    {
+    id: '1',
+    nombre: 'purificador',
+    precio: 2190,
+    descripcion: 'purifica el agua'
+    },
+
+    {
+    id: '2',
+    nombre: 'purificador de ducha',
+    precio: 1050,
+    descripcion: 'purifica al agua'
+    },
+
+    {
+    id: '3',
+    nombre: 'repuestos',
+    precio: 1090,
+    descripcion: 'repuestos de purificadores'
+    }
+]
+
+
+class Producto {
+    constructor (id, nombre, precio, descripcion){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.descripcion = descripcion
+    }
+    obtenerInfo(){
+        return `ID: ${this.id} | ${this.nombre} $${this.precio}`
+    }
+    obtenerAviso(){
+        return `${this.nombre} >>>> valgo solo $${this.precio}`
+    }
+}
+
+const carrito = []
+
+const obtenerInfoProductos = (productosArray) => {
+    return productosArray.map( (elemento) => elemento.obtenerInfo()  ).join('\n');
+}
+
+const agregarAlCarrito = (productos) => {
+    const infoProductos = obtenerInfoProductos(productos);
+    const id = prompt('Ingrese el ID del producto que desea agregar al carrito:\n' + infoProductos);
+    const producto = productos.find((producto) => producto.id === id);
+    if (!producto) return;
+    carrito.push(producto);
+    alert('Producto agregado al carrito');
+}
+
+const imprimirCarrito = (carritoDeProductos) => {
+    carritoDeProductos.forEach((producto) => {
+        console.log(producto.obtenerAviso())
+    })
+}
+
+const obtenerTotal = (productosArray) => {
+    let total = 0
+    productosArray.forEach((producto) => {
+        total += producto.precio
+    })
+    return total
+}
+
+const productos = productos_definidos.map(producto => new Producto (
+    producto.id,
+    producto.nombre,
+    producto.precio,
+    producto.descripcion,
+
+))
+
+agregarAlCarrito(productos)
+agregarAlCarrito(productos)
+agregarAlCarrito(productos)
+
+imprimirCarrito(carrito)
+console.log('El total es $',obtenerTotal(carrito))
